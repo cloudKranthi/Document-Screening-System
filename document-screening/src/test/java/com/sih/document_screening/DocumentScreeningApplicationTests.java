22730a0e68b6138510d01c7c16084f23756fbad9
cloudkranthi <shivakranthi0006@gmail.com>
2026-08-21 20:54:59 +0530

    first commit

---
+++ document-screening/src/test/java/com/sih/document_screening/DocumentScreeningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sih.document_screening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentScreeningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
